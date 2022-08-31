d3902d504e6d659ae551ca400c39f29c829325de
Shin-Shan <syu@cern.ch>
2022-08-31 14:35:29 +0200

    add scripts for luminosity calculation and producing plots

--- brilcalc_scripts/run_brilcalc_run.sh
+++ brilcalc_scripts/run_brilcalc_run.sh
@@ -2,5 +2,8 @@
 #export PATH=$HOME/.local/bin:/cvmfs/cms-bril.cern.ch/brilconda/bin:$PATH
 #pip install --user brilws
 #brilcalc --version
-brilcalc lumi  -c web -r $1 -u /pb | tail -2 | head -1 | awk '{print $12}'
+brilcalc lumi -b "STABLE BEAMS" --byls -c web -r $1 -u /pb | tail -2 | head -1 | awk '{print $12}'
+#brilcalc lumi -b "STABLE BEAMS" -c web -r $1 -u /pb | tail -2 | head -1 | awk '{print $10}'                                      
+                                 
+#brilcalc lumi -b "STABLE BEAMS" -c web -r $1 -u /pb 
 
