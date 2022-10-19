82be4bf677f0e33ae153706e54c9931ddcba4f29
Shin-Shan <syu@cern.ch>
2022-10-19 14:02:22 +0200

    add scripts to get the new runs and create csv files for the new collision runs

---
+++ runregistry_scripts/getCSV_newrun.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# first get newRuns 
+source setup_runregistry.sh
+python get_newruns.py -min $1
+
+# Then run brilcalc
+
+export PATH=$HOME/.local/bin:/cvmfs/cms-bril.cern.ch/brilconda/bin:$PATH
+pip install --user brilws
+#brilcalc --version
+
+timestamp=`date '+%Y%m%d%H%M%S'`
+echo $timestamp
+prefix=newRuns
+echo $prefix
+tempdir=$PWD/${timestamp}_${prefix}
+echo "Creating CSV files in " $tempdir
+mkdir -p $tempdir
+
+echo "Getting information without restricting to stable beams"
+
+awk -v myvar=$tempdir '{print "brilcalc lumi --byls -r",$1," -o "myvar"/"$1".csv"}' newRuns | bash
+
+cd $tempdir
+
+for file in *csv; do sed -i '/#/d' $file; done; 
+
+cd -
---
+++ runregistry_scripts/get_newruns.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python 
+import runregistry 
+import argparse 
+import sys 
+
+
+def runs_list(filter_in):  
+  runs = runregistry.get_runs(filter = filter_in) 
+  return runs 
+
+
+if __name__ == '__main__': 
+     
+    parser = argparse.ArgumentParser( 
+            description='Give list of the latest collision runs')
+    parser.add_argument("-o", "--outfile", 
+        dest="outfile", type=str, default="newRuns", help="Output file name") 
+    parser.add_argument("-min", "--min_run", dest="min_run", type=int, default=355100, help="minimum run for json") 
+    parser.add_argument("-max", "--max_run", dest="max_run",type=int, default=999999, help="maximum run for json")  
+    options = parser.parse_args() 
+
+ 
+   # generate filter  
+    filter_arg = { 'run_number': { 'and':[ {'>=': options.min_run}, {'<=': options.max_run}] },  
+                   'class': { 'like': 'Collisions22'}, 
+                   'oms_attributes.b_field': {">=": 3.7} 
+	          }
+ 
+    out_runs = runs_list(filter_arg) 
+    sys.stdout = open(options.outfile, "w")
+    for run in out_runs:  
+      print(run["run_number"])
+
+    sys.stdout.close()
