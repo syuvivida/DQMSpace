e8bef5c9ebc6efcb21fcd6a0d6d0bd235a9beb44
Shin-Shan <syu@cern.ch>
2023-04-14 11:49:02 +0200

    update the setup of environment

---
+++ runregistry_scripts/DCSonlyJSON/myMergeJSON.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+## produce input.csv
+## need to be in python 2.7 environment
+#source ./setup_json.sh 
+JSON1=$1
+JSON2=$2
+outputJSON=$3
+
+python2.7 checkJSON.py $JSON1
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+python2.7 checkJSON.py $JSON2
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+
+##############################################################################
+echo -e "\n"
+echo "Now we are going to merge the JSON files $JSON1 and $JSON2 into $outputJSON"
+python2.7 mergeJSON.py $JSON1 $JSON2 --output=$outputJSON
+
+if [ $? -ne 0 ]; then
+    echo -e "\n"
+    echo "Failed to merge JSON files using mergeJSON.py"
+    echo "Please check the JSON format or setup"
+    exit 1
+fi
+
+
+
---
+++ runregistry_scripts/DCSonlyJSON/myPrintJSON.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+## produce input.csv
+## need to be in python 2.7 environment
+JSON=$1
+#source ./setup_json.sh 
+
+python2.7 checkJSON.py $JSON
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+
+if [ $# -eq 1 ] 
+then 
+    python2.7 printJSON.py $JSON 
+elif [ $# -eq 2 ] 
+then
+    python2.7 printJSON.py $JSON --$2
+else 
+    echo -e "\n" 
+    echo "=======================================================================" 
+    echo "Usage: $scriptname jsonfile option" 
+    echo "Example: ./$scriptname $JSON or ./$scriptname $JSON option" 
+    echo "=======================================================================" 
+    echo -e "\n"  
+    ceho "options includes: range, min, max"
+    exit 1 
+fi 
+
+
+
+
+
+
+
