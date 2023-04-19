692d6bef821ffe42efa567a9e4fe14f4c400c8c7
Shin-Shan <syu@cern.ch>
2023-04-19 09:33:50 +0200

    add operation source

---
+++ runregistry_scripts/operation/setup_environment.sh
@@ -0,0 +1,7 @@
+cd /srv/node && export HOME=$PWD
+export PATH=/srv/node/bin/:$PATH
+cd /srv/
+export NODE_ENV=production
+export ENV=production
+export CLIENT_SECRET= #Ask DQM core conveners
+sudo chmod -R 777 /srv/
