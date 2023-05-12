c669cbcbdb87e50e79d713dd474f0487ca074fe7
Shin-Shan <syu@cern.ch>
2023-05-12 12:43:48 +0200

    put the client python files in the idle rather than online directory

--- onlineMachine_scripts/moveClients.sh
+++ onlineMachine_scripts/moveClients.sh
@@ -3,23 +3,32 @@
 hostname=`hostname`
 
 idledir=/etc/appliance/dqm_resources/idle/
+qrdir=/etc/appliance/dqm_resources/quarantined/
+onlinedir=/etc/appliance/dqm_resources/online/
+
 echo "-----------------------------------------------------------"
 echo "in $idledir"
 ls -1 $idledir
+cd $idledir
+sudo rm -rf *py
 
-qrdir=/etc/appliance/dqm_resources/quarantined/
 echo "-----------------------------------------------------------"
 echo "in $qrdir"
 ls -1 $qrdir
-
-onlinedir=/etc/appliance/dqm_resources/online/
-cd $onlinedir
+cd $qrdir
+sudo rm -rf *py
 
 echo "-----------------------------------------------------------"
 echo "in $onlinedir"
-ls -1 
-
+ls -1 $onlinedir
+cd $onlinedir
 sudo rm -rf *py
+
+echo "-----------------------------------------------------------"
+echo "in $idledir"
+cd $idledir
+
+
 machine=`grep -a type /etc/dqm_run_config | awk '{print $3}'`
 echo "It is a $machine machine"
 grep -a $hostname ~/clients_${machine} | awk '{print "sudo ln -s "$2" ."}' | bash
