4e2b92debc740c500075b567c4d9e97c6b82a06a
Shin-Shan <syu@cern.ch>
2023-10-11 12:00:02 +0200

    remove the requirement of --amodetag PROTPHYS

--- runregistry_scripts/lumiloss/run_brilcalc_inputcsv.sh
+++ runregistry_scripts/lumiloss/run_brilcalc_inputcsv.sh
@@ -10,10 +10,10 @@ pip install --user brilws
 mode=$3
 
 if [ "$mode" == "default" ]; then
-    brilcalc lumi -b "STABLE BEAMS" -c web --byls --amodetag PROTPHYS --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_BRIL.json -i $1 -o $2
+    brilcalc lumi -b "STABLE BEAMS" -c web --byls --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_BRIL.json -i $1 -o $2
 elif [ "$mode" == "hlt" ]; then
-    brilcalc lumi -b "STABLE BEAMS" -c web --byls --amodetag PROTPHYS --hltpath="HLT_PFJet500_*" --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_BRIL.json -i $1 -o $2
+    brilcalc lumi -b "STABLE BEAMS" -c web --byls --hltpath="HLT_PFJet500_*" --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_BRIL.json -i $1 -o $2
 else
-    brilcalc lumi -b "STABLE BEAMS" -c web --byls --amodetag PROTPHYS -i $1 -o $2
+    brilcalc lumi -b "STABLE BEAMS" -c web --byls -i $1 -o $2
 fi
 
