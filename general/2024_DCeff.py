f008c52c41db348366a993a35b37444fa1c2ebc4
Shin-Shan <syu@cern.ch>
2024-06-10 12:28:13 +0200

    update DC numbers

--- general/2024_DCeff.py
+++ general/2024_DCeff.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#declaring lists for eraB, C, and D
-lhc_delivered = [785.95,8147.51]
-cms_recorded = [693.27,7616.89] 
-DC_processed = [511.50,7613.56]
-certified = [134.73,7416.59]
-eras = ['ERA B', 'ERA C', 'ERA D']
+#declaring lists for eraB, C, D, E
+lhc_delivered = [785.95,8147.51,9119.37,3471.73]
+cms_recorded = [693.27,7616.89,8505.05, 3192.19] 
+DC_processed = [511.50,7613.56, 8321.01, 3191.36]
+certified = [134.73,7416.59, 8151.88,3138.27]
+eras = ['ERA B', 'ERA C', 'ERA D', 'ERA E']
 
 fig, ax = plt.subplots()
 
@@ -24,7 +24,8 @@ ax.set_title('2024 Performance of Data Certification per ERA')
 plt.legend((eras[0]+' - '+ str(np.round(100*certified[0]/DC_processed[0], 1))+'%', 
             eras[1]+' - '+ str(np.round(100*certified[1]/DC_processed[1], 1))+'%', 
             eras[2]+' - '+ str(np.round(100*certified[2]/DC_processed[2], 1))+'%', 
-            'BCD - '+ str(np.round(100*sum(certified)/sum(DC_processed), 1))+'%'),
+            eras[3]+' - '+ str(np.round(100*certified[3]/DC_processed[3], 1))+'%', 
+            'BCDE - '+ str(np.round(100*sum(certified)/sum(DC_processed), 1))+'%'),
             scatterpoints=1,
             loc='lower left',
             ncol=2,
@@ -32,7 +33,7 @@ plt.legend((eras[0]+' - '+ str(np.round(100*certified[0]/DC_processed[0], 1))+'%
 #plt.show()
 plt.savefig('2024_eras_DC_performance.png')
 
-print('The sum of lhc_delivered is ', sum(lhc_delivered), '/fb')
-print('The sum of CMS_recorded is ', sum(cms_recorded), '/fb')
-print('The sum of DC_processed is ', sum(DC_processed), '/fb')
-print('The sum of certified is ', sum(certified), '/fb')
+print('The sum of lhc_delivered is ', sum(lhc_delivered), '/pb')
+print('The sum of CMS_recorded is ', sum(cms_recorded), '/pb')
+print('The sum of DC_processed is ', sum(DC_processed), '/pb')
+print('The sum of certified is ', sum(certified), '/pb')
--- general/2024_Fraction.py
+++ general/2024_Fraction.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-lhc_delivered = [785.95,8147.51]
-cms_recorded = [693.27,7616.89] 
-DC_processed = [511.50,7613.56]
-certified = [134.73,7416.59]
-eras = ['ERA B', 'ERA C', 'ERA D']
+#declaring lists for eraB, C, D, E
+lhc_delivered = [785.95,8147.51,9119.37,3471.73]
+cms_recorded = [693.27,7616.89,8505.05, 3192.19] 
+DC_processed = [511.50,7613.56, 8321.01, 3191.36]
+certified = [134.73,7416.59, 8151.88,3138.27]
+eras = ['ERA B', 'ERA C', 'ERA D', 'ERA E']
 
 fig, ax = plt.subplots()
 
@@ -23,7 +24,8 @@ ax.set_title('2024 Fraction of Recorded Data for Physics Analysis per ERA')
 plt.legend((eras[0]+' - '+ str(np.round(100*certified[0]/cms_recorded[0], 1))+'%', 
             eras[1]+' - '+ str(np.round(100*certified[1]/cms_recorded[1], 1))+'%', 
             eras[2]+' - '+ str(np.round(100*certified[2]/cms_recorded[2], 1))+'%', 
-            'BCD - '+ str(np.round(100*sum(certified)/sum(cms_recorded), 1))+'%'),
+            eras[3]+' - '+ str(np.round(100*certified[3]/cms_recorded[3], 1))+'%', 
+            'BCDE - '+ str(np.round(100*sum(certified)/sum(cms_recorded), 1))+'%'),
             scatterpoints=1,
             loc='lower left',
             ncol=2,
@@ -31,7 +33,7 @@ plt.legend((eras[0]+' - '+ str(np.round(100*certified[0]/cms_recorded[0], 1))+'%
 #plt.show()
 plt.savefig('2024_eras_phy_performance.png')
 
-print('The sum of lhc_delivered is ', sum(lhc_delivered), '/fb')
-print('The sum of CMS_recorded is ', sum(cms_recorded), '/fb')
-print('The sum of DC_processed is ', sum(DC_processed), '/fb')
-print('The sum of certified is ', sum(certified), '/fb')
+print('The sum of lhc_delivered is ', sum(lhc_delivered), '/pb')
+print('The sum of CMS_recorded is ', sum(cms_recorded), '/pb')
+print('The sum of DC_processed is ', sum(DC_processed), '/pb')
+print('The sum of certified is ', sum(certified), '/pb')
