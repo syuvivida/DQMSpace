b7ebd2cb05a43b45472d870673ac5a6ea298caaf
Shin-Shan Yu <yush@HANN10101-LT.local>
2026-09-02 09:18:49 -0400

    update to 2024

--- json_tools/splitJSON_byEras.py
+++ json_tools/splitJSON_byEras.py
@@ -1,20 +1,22 @@
 import json
 
-with open('Cert_Collisions2022_355100_362760_Golden.json') as f:
+with open('Cert_Collisions2024_378981_386951_Golden.json') as f:
     data = json.load(f)
 
 eras = {
-    'CtoG': (355794, 362760),
-    'C': (355794, 357486),
-    'D': (357487, 359021),
-    'E': (359022, 360331),
-    'F': (360332, 362180),
-    'G': (362350, 362760),
+    'CtoI': (379412,387121),
+    'C': (379412, 380252),
+    'D': (380253, 380947),
+    'E': (380948, 381943),
+    'F': (381944, 383779),
+    'G': (383780, 385813),
+    'H': (385814, 386408),
+    'I': (386409, 387121)
 }
 
 for era, (lo, hi) in eras.items():
     subset = {k: v for k, v in data.items() if lo <= int(k) <= hi}
-    out = f'Cert_Collisions2022_Era{era}_Golden.json'
+    out = f'Cert_Collisions2024_Era{era}_Golden.json'
     with open(out, 'w') as f:
         json.dump(subset, f, indent=4)
     print(f"Era {era} ({lo}–{hi}): {len(subset)} runs → {out}")
